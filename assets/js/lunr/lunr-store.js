21218e84f345b8aa59f5471c12383d0a723305b9
Blest215 <blest215@kaist.ac.kr>
2020-08-26 08:00:33 +0000

    Site updated at 2020-08-26 08:00:33 UTC

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Spatially Cohesive Service Discovery and Dynamic Service Handover for Distributed IoT Environments",
+        "excerpt":"This is my first paper published, that I have presented at International Conference on Web Engineering (ICWE) 2017 conference when I was in the second year of the master’s. To present this paper, I have been to Rome for my first abroad business trip. Afterwards, I attended ICWE every year,...","categories": ["Paper"],
+        "tags": ["ServiceDiscovery","ServiceHandover","IoT","ICWE"],
+        "url": "https://blest215.github.io/paper/ICWE2017/",
+        "teaser": "https://blest215.github.io/assets/images/bonfire.jpg"
+      },{
+        "title": "Developing Service Agent using Python Decorators",
+        "excerpt":"When I participated in different IoT testbed development project, using Java annotations were very impressive. Within appropriate annotations, information of the service agents were automatically registered to the server, and also UI was generated to let the people use. So, I want to use such functionalities in our lab’s IoT...","categories": ["Development"],
+        "tags": ["Python","Service","Testbed"],
+        "url": "https://blest215.github.io/development/PythonDecorator/",
+        "teaser": "https://blest215.github.io/assets/images/bonfire.jpg"
+      }]
---
+++ ko/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Spatially Cohesive Service Discovery and Dynamic Service Handover for Distributed IoT Environments",
+        "excerpt":"내가 석사 2년차 때 International Conference on Web Engineering (ICWE) 2017 학회에 발표했던 첫 논문이다. 이 논문을 발표하기 위해 로마에 첫 해외 학회 출장을 다녀온 후, 코로나 때문에 온라인 참석한 올해를 제외하고 ICWE 2017 (이탈리아 로마), 2018 (스페인 카세레스), 2019 (대한민국 대전)에 매년 논문을 내며 참석했고, 매번 잊지 못할 좋은...","categories": ["Paper"],
+        "tags": ["ServiceDiscovery","ServiceHandover","IoT","ICWE"],
+        "url": "https://blest215.github.io/paper/ICWE2017/",
+        "teaser": "https://blest215.github.io/assets/images/bonfire.jpg"
+      },{
+        "title": "파이썬 decorator를 사용한 서비스 에이전트 개발",
+        "excerpt":"예전에 다른 IoT 테스트베드 개발 프로젝트에 참여했을 때, Java의 annotation을 잘 활용해서 서비스 에이전트에 적절한 annotation을 달면 자동으로 에이전트의 정보가 서버에 등록되어 사람들이 활용할 수 있는 UI까지 자동으로 생성해주는 기능이 인상적이었다. 그래서 우리 연구실의 테스트베드 개발에도 이처럼 에이전트의 메타 정보를 편하게 관리할 수 있도록 하고 싶었고, Python에서 지원하는 decorator를 적극적으로...","categories": ["Development"],
+        "tags": ["Python","Service","Testbed"],
+        "url": "https://blest215.github.io/development/PythonDecorator/",
+        "teaser": "https://blest215.github.io/assets/images/bonfire.jpg"
+      }]
